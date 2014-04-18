04350d965268272715017f559a67683d3a1cc645
Jack Lot <jacklotkowski@gmail.com>
2014-04-18 01:11:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockerator::Application.config.secret_key_base = '813db97b24808fe83e7b5cf55eaf4952ae436173dc516511658ee171f2ad55996063dcdc40ca409ceb218cfabbb86cccca27683a6d6683dfea1779de7e264064'
